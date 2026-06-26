d99546ead5b296ddb6fca73ce167106ccee891f6
llikkill338-arch <llikkill338@gmail.com>
2026-06-26 19:08:12 +0800

    feat: screens.h v2

--- src/screens.h
+++ src/screens.h
@@ -1,6 +1,5 @@
 // ============================================================================
-// screens.h — Экраны и игровые режимы
-// Модуль: меню, заставка, настройки, игровые циклы
+// screens.h - Все экраны и игровые режимы
 // ============================================================================
 
 #ifndef SCREENS_H
@@ -9,43 +8,17 @@
 #include "utils.h"
 #include "graphics.h"
 #include "game_logic.h"
-#include "bot_ai.h"
-#include <string>
 
-/** Заставка с ASCII-арт логотипом и анимацией загрузки */
 void showLogo();
-
-/** Главное меню: устанавливает selected в индекс выбранного пункта (0-5) */
 void showMainMenu(int& selected);
-
-/** Экран правил / Об игре */
 void showRules();
-
-/** Настройки звука */
-void showSoundSettings();
-
-/** Подтверждение выхода: true если пользователь подтвердил */
-bool confirmExit();
-
-/** Экран передачи хода с 3-секундным обратным отсчётом */
+void showSettings(bool& sound);
+void confirmExit();
 void switchScreenWithTimer(const std::string& nextPlayerName);
 
-/** Показать сообщение на экране в указанной строке */
-void showMessage(const std::string& text, Color color, int row);
+// Режимы
+void playBotMode(int f1[R][R], int f2[R][R], int level, bool autoPlace);
+void playPVPTurn(int f1[R][R], int f2[R][R]);
+void playPVPSplit(int f1[R][R], int f2[R][R]);
 
-/** Экран победы */
-void showVictoryScreen(const std::string& winner);
-
-/** Экран поражения */
-void showDefeatScreen(const std::string& loser);
-
-/** Режим "Бой с ботом". botLevel: 0 = лёгкий, 1 = сложный */
-bool playBotMode(int field1[R][R], int field2[R][R], int botLevel, bool autoPlaceP1);
-
-/** Режим "1 на 1 по очереди" */
-bool playPVP_TurnMode(int p1Field[R][R], int p2Field[R][R], int p1Fog[R][R], int p2Fog[R][R]);
-
-/** Режим "1 на 1 разделённый экран" */
-bool playPVP_SplitMode(int p1Field[R][R], int p2Field[R][R], int p1Fog[R][R], int p2Fog[R][R]);
-
-#endif // SCREENS_H
+#endif
